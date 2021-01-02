2bf3fb22e04db97e498b04fdb7c32694336028e8
Taha Azzaoui <taha@azzaoui.org>
2021-01-01 21:15:49 -0800

    add selection functions

--- perms.h
+++ perms.h
@@ -3,7 +3,19 @@
 
 extern const int p_0[];
 extern const int p_1[];
+extern const int p_2[];
 extern const int key_sched[];
+extern const int e_table[];
+
+extern const int s_1[4][16];
+extern const int s_2[4][16];
+extern const int s_3[4][16];
+extern const int s_4[4][16];
+extern const int s_5[4][16];
+extern const int s_6[4][16];
+extern const int s_7[4][16];
+extern const int s_8[4][16];
+
 uint64_t permute(uint64_t, const int *, int);
 
 #endif                          /* PERMS_H */
